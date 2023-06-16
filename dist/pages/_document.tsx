0ae350266da0448609af3aca1e93ab2d22a7f25b
Felipe Oliveira <felipephito@gmail.com>
2023-06-16 18:56:06 -0300

    feat: Creates pages structure

--- dist/pages/_document.tsx
+++ dist/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className='bg-white text-gray-dark'>
+      <body>
         <Main />
         <NextScript />
       </body>
