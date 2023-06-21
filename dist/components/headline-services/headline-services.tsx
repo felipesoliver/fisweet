be0f01ee339f28b129a5e26a251ff5bbd73f55d9
Felipe Oliveira <felipephito@gmail.com>
2023-06-20 22:28:34 -0300

    chore: Changes headline-services animation

--- dist/components/headline-services/headline-services.tsx
+++ dist/components/headline-services/headline-services.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import Fade from 'react-reveal/Fade'
 import Link from 'next/link'
 
 import servicesCardOne from '~/assets/images/services-card1.png'
@@ -67,42 +68,64 @@ const HeadlineServices = () => {
           <p className='max-w-[46rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.</p>
         </div>
         {cards.length > 0 && (
-          <Swiper
-            className='col-span-full w-full overflow-visible pb-16'
-            slidesPerView={1.1}
-            spaceBetween={24}
-            pagination={{
-              clickable:true
-            }}
-            modules={[Pagination]}
-            breakpoints={{
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-            }}
-          >
-            {cards.map((item: any, index: any) => (
-              <SwiperSlide key={`card-${index}`}>
-                <article className='group lg:col-span-4'>
-                  <Link href={item.cta.url}>
-                    <figure className='w-full h-[15.125rem] rounded-2xl overflow-hidden'>
-                      <Image className='w-full h-full object-cover group-hover:lg:scale-110 duration-300' {...item.image} />
-                    </figure>
-                    <div className='pt-8 space-y-6'>
-                      <h3 className='text-2xl'>{item.title}</h3>
-                      <p>{item.description}</p>
-                      <button className='flex items-center gap-x-2 text-orange font-medium'><span>Learn More</span><ArrowRight className="group-hover:lg:translate-x-1 duration-200" /></button>
-                    </div>
-                  </Link>
-                </article>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <>
+            <Swiper
+              className='lg:hidden col-span-full w-full overflow-visible pb-16'
+              slidesPerView={1.1}
+              spaceBetween={24}
+              pagination={{
+                clickable:true
+              }}
+              modules={[Pagination]}
+              breakpoints={{
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+              }}
+            >
+              {cards.map((item: any, index: any) => (
+                <SwiperSlide key={`card-${index}`}>
+                  <article className='group lg:col-span-4'>
+                    <Link href={item.cta.url}>
+                      <figure className='w-full h-[15.125rem] rounded-2xl overflow-hidden'>
+                        <Image className='w-full h-full object-cover group-hover:lg:scale-110 duration-300' {...item.image} />
+                      </figure>
+                      <div className='pt-8 space-y-6'>
+                        <h3 className='text-2xl'>{item.title}</h3>
+                        <p>{item.description}</p>
+                        <button className='flex items-center gap-x-2 text-orange font-medium'><span>Learn More</span><ArrowRight className="group-hover:lg:translate-x-1 duration-200" /></button>
+                      </div>
+                    </Link>
+                  </article>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className='hidden lg:block col-span-full'>
+              <Fade bottom cascade distance="100px">
+                <div className='grid grid-cols-12 gap-x-5'>
+                  {cards.map((item: any, index: any) => (
+                    <article className='group lg:col-span-4' key={`card-${index}`}>
+                      <Link href={item.cta.url}>
+                        <figure className='w-full h-[15.125rem] rounded-2xl overflow-hidden'>
+                          <Image className='w-full h-full object-cover group-hover:lg:scale-110 duration-300' {...item.image} />
+                        </figure>
+                        <div className='pt-8 space-y-6'>
+                          <h3 className='text-2xl'>{item.title}</h3>
+                          <p>{item.description}</p>
+                          <button className='flex items-center gap-x-2 text-orange font-medium'><span>Learn More</span><ArrowRight className="group-hover:lg:translate-x-1 duration-200" /></button>
+                        </div>
+                      </Link>
+                    </article>
+                  ))}
+                </div>
+              </Fade>
+            </div>
+          </>
         )}
       </Grid>
     </section>
