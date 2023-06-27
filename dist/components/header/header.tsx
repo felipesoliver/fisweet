005eafaa83d518772138e2ee2a5cfc53c1dd52c1
Felipe Oliveira <felipephito@gmail.com>
2023-06-27 15:10:11 -0300

    feat: Improvees mobile header

--- dist/components/header/header.tsx
+++ dist/components/header/header.tsx
@@ -74,16 +74,23 @@ const Header = () => {
           <Link className='font-semibold btn-orange-light' href="/clone-project">Clone project</Link>
         </nav>
         <Jump left cascade when={active} appear>
-          <nav className={classNames('mobile-nav-menu fixed left-0 w-screen h-screen bg-white gap-y-8 flex flex-col p-10 md:p-16 duration-300', {
+          <nav className={classNames('mobile-nav-menu fixed left-0 w-screen h-screen bg-white gap-y-8 flex flex-col justify-center p-10 md:p-16 duration-300', {
             'bottom-0': active,
             '-bottom-[100vh]': !active,
           })}>
-              <Link className='font-semibold text-3xl md:text-5xl btn-nav' href="/about">About Us</Link>
-              <Link className='font-semibold text-3xl md:text-5xl btn-nav' href="/careers">Careers</Link>
-              <Link className='font-semibold text-3xl md:text-5xl btn-nav' href="/services">Services</Link>
-              <Link className='font-semibold text-3xl md:text-5xl btn-nav' href="/blog">Blog</Link>
-              <Link className='font-semibold text-3xl md:text-5xl btn-nav' href="/contact-us">Contact us</Link>
-              <Link className='font-semibold btn-orange-light' href="/clone-project">Clone project</Link>
+            {headerMenu.length > 0 &&
+              headerMenu.map((link: any, index: any) => (
+                <Link
+                  key={`link-${index}`}
+                  className={classNames('font-semibold text-3xl md:text-5xl btn-nav', {
+                  'active': router.asPath.includes(link.url)})}
+                  href={link.url}
+                  onClick={() => setActive(false)}
+                >
+                  {link.title}
+                </Link>
+            ))}
+            <Link className='font-semibold btn-orange-light' href="/clone-project">Clone project</Link>
           </nav>
         </Jump>
       </div>
