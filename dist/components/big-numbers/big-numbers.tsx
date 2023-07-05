af75aadf15a28572b9a83979d5ecba3fa9878dfb
Felipe Oliveira <felipephito@gmail.com>
2023-07-05 11:35:37 -0300

    feat: Finishes big-numbers component

--- dist/components/big-numbers/big-numbers.tsx
+++ dist/components/big-numbers/big-numbers.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Fade from '@successtar/react-reveal/Fade'
+import { CountUp } from 'use-count-up'
+import { onScroll } from '~/utils/scroll'
+import { data } from './data'
+import Grid from '~/components/grid'
 
 const BigNumbers = () => {
+  const targetReference = useRef(null)
+  const [isCountActive, setIsCountActive] = useState(false)
+
+  useEffect(() => {
+    return onScroll((scrollPosition) => {
+      scrollPosition >= targetReference.current.offsetTop - targetReference.current.clientHeight
+        ? setIsCountActive(true)
+        : setIsCountActive(false)
+    })
+  }, [targetReference, isCountActive])
+
   return (
-    <div>BigNumbers</div>
+    <section className='big-numbers pb-10' ref={targetReference}>
+      <Grid className='container gap-y-10 lg:gap-y-20'>
+        <div className='col-span-full lg:col-span-6 lg:col-start-7'>
+          <Fade>
+            <p>{data.description}</p>
+          </Fade>
+        </div>
+        <ul className='col-span-full flex flex-col lg:flex-row gap-5'>
+          {data.numbers.map((item: any, index: any) => (
+            <li className='space-y-4' key={`item-${index}`}>
+              <span className='text-4xl font-semibold text-orange'><CountUp isCounting={isCountActive} duration={5} end={item.number} />+</span>
+              <h3 className='text-2xl font-semibold'>{item.title}</h3>
+              <p>{item.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Grid>
+    </section>
   )
 }
 
--- dist/components/big-numbers/data.ts
+++ dist/components/big-numbers/data.ts
@@ -1,6 +1,25 @@
 export const data = {
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.',
   numbers: [
-    {}
+    {
+      number: 15,
+      title: 'Awards received',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.'
+    },
+    {
+      number: 500,
+      title: 'Clients served',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.'
+    },
+    {
+      number: 34,
+      title: 'Employees',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.'
+    },
+    {
+      number: 130,
+      title: 'Custom solutions',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.'
+    },
   ]
 }
